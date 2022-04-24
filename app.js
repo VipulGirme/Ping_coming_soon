430519369ca6d62cd15206462cb7a8cdba0c6558
Vipul R Girme <49040675+VipulGirme@users.noreply.github.com>
2022-04-24 23:42:37 +0530

    JS commit

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const validation = (e) => {
-    console.log('Clicked')
+    var input = document.getElementsByTagName("input")[0]
+    var pattern = /([a-zA-Z0-9]+)\@([a-zA-Z]+)\.([a-zA-Z]{3})/;
+
+    if (input.value != "" && input.value.match(pattern)) {
+        alert("subimitted")
+    }
+    else {
+        input.style.borderColor = "hsl(354, 100%, 66%)";
+        input.placeholder = "example@email.com";
+        document.getElementById("error-msg").innerHTML = "Please provide a valid email address";
+        document.getElementById("error-msg").style.fontSize = '12px';
+        document.getElementById("error-msg").style.color = 'red'
+        return false
+    }
+
 }
